f4040c5e38376e7eee6d3dcefd9961c924aef39d
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-09-21 16:42:54 +0100

    🗃️ update company schema

--- backend/graphql/resolvers/company.ts
+++ backend/graphql/resolvers/company.ts
@@ -3,25 +3,25 @@ import { resolverType } from 'fast-graphql';
 import * as schema from '@/backend/graphql/generated/schemaType';
 
 const Query = {
-  async read(parent: any, args: any, ctx: any): Promise<schema.Query['read']> {
+  async readCompany(parent: any, args: any, ctx: any): Promise<schema.Query['readCompany']> {
     return await companyController().read(args);
   },
   async listCompany(parent: any, args: any, ctx: any): Promise<schema.Query['listCompany']> {
     return await companyController().list(args);
   },
-  async search(parent: any, args: any, ctx: any): Promise<schema.Query['search']> {
+  async searchCompany(parent: any, args: any, ctx: any): Promise<schema.Query['searchCompany']> {
     return await companyController().search(args);
   },
 };
 
 const Mutation = {
-  async create(parent: any, args: any, ctx: any): Promise<schema.Mutation['create']> {
+  async createCompany(parent: any, args: any, ctx: any): Promise<schema.Mutation['createCompany']> {
     return await companyController().create(args);
   },
-  async update(parent: any, args: any, ctx: any): Promise<schema.Mutation['update']> {
+  async updateCompany(parent: any, args: any, ctx: any): Promise<schema.Mutation['updateCompany']> {
     return await companyController().update(args);
   },
-  async delete(parent: any, args: any, ctx: any): Promise<schema.Mutation['delete']> {
+  async deleteCompany(parent: any, args: any, ctx: any): Promise<schema.Mutation['deleteCompany']> {
     return await companyController().delete(args);
   },
 };
--- backend/models/Company.ts
+++ backend/models/Company.ts
@@ -3,12 +3,16 @@ import * as schemaType from '@/backend/graphql/generated/schemaType';
 
 // 2. Create a Schema corresponding to the document interface.
 const schema = new Schema<schemaType.Company>({
+  lang: { type: String, required: true , default:"fr" },
   companyName: { type: String, required: true },
   logo: { type: String, },
   managerName: { type: String, required: true },
   activity: { type: String, required: true },
-  sectorActivity: { type: String, required: true },
+  sectorActivity: { type: String, required: true , default:"printing_house" ,lowercase:true},
+  products: { type: Array , default:[]},
+  country:{ type: String, required: true ,default:"DZ" , uppercase:true },
   state:{ type: String, required: true },
+  city:{ type: String, required: true },
   address:{ type: String, required: true },
   phone:{ type: String,  },
   cell:{ type: String,  },
@@ -16,6 +20,7 @@ const schema = new Schema<schemaType.Company>({
   website:{ type: String,  },
   facebook:{ type: String,  },
   services:{ type: String, },
+  slug:{ type: String, required: true},
   created: {
     type: Date,
     default: Date.now,
