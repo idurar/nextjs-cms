a6dcb6a4e03d40c3ab1905f63a8bd88eafb9aeb0
polymahh <otman.elkantaoui@gmail.com>
2023-09-26 14:54:44 +0100

    feat: creating detail page

--- components/CompanyCard.tsx
+++ components/CompanyCard.tsx
@@ -1,15 +1,65 @@
 import React from 'react';
 import { Card, Typography, Button } from 'antd';
 import { HeartOutlined, StarOutlined, GithubOutlined, ArrowsAltOutlined } from '@ant-design/icons';
-
-const { Paragraph } = Typography;
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { setCurrentCompany } from '@/redux/features/company-slice';
+import { AppDispatch } from '@/redux/store';
 
 const CompanyCard = ({ item }: { item: any }) => {
+  const { Paragraph } = Typography;
+  const router = useRouter();
+
+  const dispatch = useDispatch<AppDispatch>();
+  const Action = ({
+    text,
+    icon,
+    onClick = () => {},
+  }: {
+    text: string;
+    icon: React.ReactNode;
+    onClick?: () => void;
+  }) => {
+    return (
+      <div className="actionContainer" onClick={onClick}>
+        <div className="actionIcon">
+          {icon}
+          <div className="actionText">{text}</div>
+        </div>
+      </div>
+    );
+  };
   return (
     <Card
-      title={item?.companyName}
+      title={item?.companyName || 'Company name'}
       className="repoCard"
-      style={{ minHeight: location.pathname != '/' ? '500px' : 'auto' }}
+      style={{ minHeight: router.pathname != '/' ? '500px' : 'auto' }}
+      actions={[
+        router.pathname == '/' ? (
+          <Action
+            text={'Open'}
+            key="openCard"
+            icon={<ArrowsAltOutlined />}
+            onClick={() => {
+              if (item) {
+                console.log('🚀 ~ file: CompanyCard.tsx:25 ~ CompanyCard ~ open:');
+                dispatch(setCurrentCompany(item));
+              }
+              router.push(`company/${item?.companyName}`);
+            }}
+          />
+        ) : (
+          <Action
+            text={'Close'}
+            key="closeCard"
+            icon={<ArrowsAltOutlined />}
+            onClick={() => {
+              router.push('/');
+            }}
+          />
+        ),
+      ]}
     >
       <Paragraph ellipsis={{ rows: 2 }}>
         Link : <a href={item?.website}>{item?.website}</a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,16 @@ import type { AppProps } from 'next/app';
 
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '@/apollo';
+import { DefaultLayout } from '@/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
     </ApolloProvider>
   );
 }
